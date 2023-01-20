a219f38744752203538553a7df74551cb59a572e
Azarudeen <76832585+azarcoder@users.noreply.github.com>
2023-01-20 14:53:01 +0530

    Update 14.py

--- T4TEQ/functions/14.py
+++ T4TEQ/functions/14.py
@@ -1,16 +1,23 @@
 n=int(input())
+
 def pm(n):
     f=True
-    for i in range(2,n+1):
+    if n==1:
+        return False
+    for i in range(2,n//2+1):
         if(n%i==0):
             f=False
+            break 
+    return f
+        
+def spl(n):
+    f=False
+    for i in range(1,n//2+1):
+        if(pm(i) and pm(n-i)):
+            f=True
             break
-        if(f):
-            return n 
+    return f
+print(spl(n))
 
-for i in range(2,n//2+1):
-    for j in range(2,n//2+1):
-        if pm(i)+pm(j)==n:
-            break
-             
-
+            
+            
