8110c75fd422a9548b7e2fe77fc5f117c02424ca
Azarudeen <76832585+azarcoder@users.noreply.github.com>
2023-01-26 11:24:28 +0530

    Delete sort string without list.py

--- T4TEQ/String/sort string without list.py
+++
@@ -1,7 +0,0 @@
-s=input()
-#print(sorted(s)) returns as list
-for i in range(len(s)):
-    for j in range(i+1,len(s)):
-        if s[i]>s[j]:
-            s=s[:i]+s[j]+s[i+1:j]+s[i]+s[j+1:]
-print(s)
