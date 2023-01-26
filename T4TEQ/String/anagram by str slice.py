631bbf0bedc6b7c4b6b224695f1f3506324b188d
Azarudeen <76832585+azarcoder@users.noreply.github.com>
2023-01-26 11:23:07 +0530

    Add files via upload

---
+++ T4TEQ/String/anagram by str slice.py
@@ -0,0 +1,8 @@
+a,b=input(),input()
+def st(s):
+    for i in range(len(s)):
+        for j in range(i+1,len(s)):
+            if s[i]>s[j]:
+                s=s[:i]+s[j]+s[i+1:j]+s[i]+s[j+1:]
+    return s
+print(st(a)==st(b) and "anagram" or "not")
---
+++ T4TEQ/String/sort without fn.py
@@ -0,0 +1,11 @@
+s=input()
+e=''
+l=len(s)
+i=1
+d=ord(s[0])
+while(i<l):
+    if ord(s[i]) < d:
+        d=ord(s[i])
+        e=e.replace
+    i+=1
+print(e)
