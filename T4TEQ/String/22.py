c535a4ed6e43164c24aa88aa9fd316453a598af2
Azarudeen <76832585+azarcoder@users.noreply.github.com>
2023-01-30 17:01:10 +0530

    Update 22.py

--- T4TEQ/String/22.py
+++ T4TEQ/String/22.py
@@ -1,8 +1,8 @@
 n,f,r=input(),input(),input()
 k=0
 for i in n.split():
-     k+=1
-     if i==f:
-          print(k%2 and r or i,end=" ")
-     else:
-          print(i,end=" ")
+    if i==f:
+        k+=1
+        print(k%2 and r or i,end=' ')
+    else:
+        print(i,end=' ')                  
